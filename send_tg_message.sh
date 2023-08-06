b4e93177aac946b60a5469aa9f74bdfa5e679e92
fukiame <fukiame@proton.me>
2023-08-06 20:39:34 +0700

    hfkakcnjdiabxb

--- send_tg_message.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
-  -d chat_id="$CHAT_ID" \
-  -d "disable_web_page_preview=true" \
-  -d "parse_mode=html" \
-  -d text="$1"
---
+++ tg_utils.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+case $1 in
+    "msg" )
+        curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+            -d chat_id="$CHAT_ID" \
+            -d "disable_web_page_preview=true" \
+            -d "parse_mode=html" \
+            -d text="$2"
+    ;;
+    "up" | "upload" )
+        curl -F chat_id="$CHAT_ID" \
+            -F document=@"$2" \
+            -F parse_mode=markdown https://api.telegram.org/bot$BOT_TOKEN/sendDocument \
+            -F caption="$3"
+    ;;
+esac
