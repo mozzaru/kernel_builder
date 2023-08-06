32036ce6cc5012d04e6806496edc72964cb58a12
fukiame <fukiame@proton.me>
2023-08-07 02:37:58 +0700

    feat: preliminary support for batch compiling
    the script can already handle that since the refactor, this commit only
    add the nessecary mechanics to upload those

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -15,9 +15,11 @@ cd kernel
 bash ../build.sh "$SCRIPTS"
 
 ZIP=$(echo *.zip)
-if [ -e $ZIP ]; then
+if [[ -e $ZIP ]]; then
+  for file in $(find . -name '*.zip' -maxdepth 1); do
     bash ../tg_utils.sh up "$ZIP" ""
+  done
 else
-    bash ../tg_utils.sh up "out/error.log" ""
+  bash ../tg_utils.sh up "out/error.log" ""
 fi
 
