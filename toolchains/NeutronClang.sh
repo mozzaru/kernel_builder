5d6a69d3fd15b1ed0b1210442894a4637f112f9e
fukiame <fukiame@proton.me>
2023-08-07 03:28:39 +0700

    )))

--- toolchains/NeutronClang.sh
+++ toolchains/NeutronClang.sh
@@ -21,22 +21,22 @@ case $1 in
         make -j$(nproc --all) O=out ARCH=arm64 SUBARCH=arm64 $2
         make -j$(nproc --all) O=out \
             PATH="${dir}/bin:/usr/bin:${PATH}" \
-                CROSS_COMPILE="aarch64-linux-gnu-"
-                CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
-                CROSS_COMPILE_COMPAT="arm-linux-gnueabi-"
-                CC=clang \
-                LD=ld.lld \
-                NM=llvm-nm \
-                AR=llvm-ar \
-                STRIP=llvm-strip \
-                OBJCOPY=llvm-objcopy \
-                OBJDUMP=llvm-objdump \
-                READELF=llvm-readelf \
-                LLVM_IAS=1 \
-                HOSTCC=clang \
-                HOSTCXX=clang++ \
-                HOSTLD=ld.lld \
-                HOSTAR=llvm-ar \
-                2>&1 | tee out/error.log
+            CROSS_COMPILE="aarch64-linux-gnu-" \
+            CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+            CROSS_COMPILE_COMPAT="arm-linux-gnueabi-" \
+            CC=clang \
+            LD=ld.lld \
+            NM=llvm-nm \
+            AR=llvm-ar \
+            STRIP=llvm-strip \
+            OBJCOPY=llvm-objcopy \
+            OBJDUMP=llvm-objdump \
+            READELF=llvm-readelf \
+            LLVM_IAS=1 \
+            HOSTCC=clang \
+            HOSTCXX=clang++ \
+            HOSTLD=ld.lld \
+            HOSTAR=llvm-ar \
+            2>&1 | tee out/error.log
     ;;
 esac
