de374bd0326cf861353a4e2b917fd00bf1790fac
fukiame <fukiame@proton.me>
2023-08-07 09:08:35 +0700

    pls work wo python2 pls pls pls pls pls pls pls pls pls pls

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,7 @@ apt-get install llvm lld bc bison ca-certificates curl flex gcc git libc6-dev \
   libssl-dev openssl python3 zip zstd make clang gcc-arm-linux-gnueabi \
   software-properties-common device-tree-compiler libxml2 libarchive-tools \
   libelf-dev libssl-dev libtfm-dev wget xz-utils -y
+ln -sf "/usr/bin/python3" /usr/bin/python
 
 bash tg_utils.sh msg "gh $RUN_NUM: cloning kernel source, repo: $REPO"
 git clone --depth=1 https://github.com/$REPO -b $KERNEL_BRANCH kernel
