4aaba63b7ea763f7f515cd080ca3cb05252960f2
fukiame <fukiame@proton.me>
2023-08-07 09:42:21 +0700

    fix: move to Ubuntu Kinetic
    this is the only distro i know that has both the required libc version
    to run some newer clang / gcc builds and has python2 support
    
    newer Ubuntu releases and Debian Bookworm completely dropped python2 support
    Debian Bullseye and Ubuntu releases older than Kinetic doesnt have the
    libc version requied

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get install llvm lld bc bison ca-certificates curl flex gcc git libc6-dev \
-  libssl-dev openssl python3 zip zstd make clang gcc-arm-linux-gnueabi \
+  libssl-dev openssl python3 python2 zip zstd make clang gcc-arm-linux-gnueabi \
   software-properties-common device-tree-compiler libxml2 libarchive-tools \
   libelf-dev libssl-dev libtfm-dev wget xz-utils -y
 ln -sf "/usr/bin/python3" /usr/bin/python
