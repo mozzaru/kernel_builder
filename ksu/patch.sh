1a2b7570cffaa68e64cc2160acb23458dffa1d3c
fukiame <fukiame@proton.me>
2023-08-30 20:31:32 +0700

    feat: ksu patching: append ksu to CONFIG_LOCALVERSION

--- ksu/patch.sh
+++ ksu/patch.sh
@@ -5,6 +5,14 @@ curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh
 KSU_GIT_VERSION=$(cd KernelSU && git rev-list --count HEAD)
 KERNELSU_VERSION=$(($KSU_GIT_VERSION + 10000 + 200))
 
+defconfig="selene_defconfig"
+defconfig_file="${maindir}/arch/arm64/configs/${defconfig}"
+
+KERNEL_NAME=$(cat "${defconfig_file}" | grep "CONFIG_LOCALVERSION=" | sed 's/CONFIG_LOCALVERSION="-*//g' | sed 's/"*//g' )
+sed -i "/CONFIG_LOCALVERSION=\"/s/.$/$KERNEL_NAME-KSU-$KERNELSU_VERSION\"/" "${defconfig_file}"
+
+echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
+
 echo "includes KernelSU $KERNELSU_VERSION" >> banner_append
 
 for patch_file in ${outside}/ksu/patches/*.patch ; do
