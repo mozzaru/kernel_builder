c1c4683b555861936caa06313337b76feb4e822d
fukiame <fukiame@proton.me>
2023-09-04 09:25:29 +0700

    jfnsifbejj

--- ksu/patch.sh
+++ ksu/patch.sh
@@ -5,15 +5,15 @@
 source env
 
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
-KSU_GIT_VERSION=$(cd KernelSU && git rev-list --count HEAD)
-KERNELSU_VERSION=$(($KSU_GIT_VERSION + 10000 + 200))
+KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
+KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
-sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-KSU-$KERNELSU_VERSION\"/" "${defconfig_file}"
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-KSU${KSU_ver}\"/" "${defconfig_file}"
 
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
 
-echo "includes KernelSU $KERNELSU_VERSION" >> banner_append
+echo "includes KernelSU ${KSU_ver}" >> banner_append
 
 for patch_file in ${outside}/ksu/patches/*.patch ; do
-  patch -p1 --no-backup-if-mismatch < "$patch_file"
+  patch -p1 < "$patch_file"
 done
