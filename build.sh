d63f4b2fbecf18be73a74c0fe203843a925592f3
fukiame <fukiame@proton.me>
2023-09-04 08:13:23 +0700

    lqkcbdhjqcDxu

--- build.sh
+++ build.sh
@@ -60,10 +60,11 @@ for toolchain in $1; do
   if [ -e "${out_image}" ]; then
     BUILD_END=$(date +"%s")
     DIFF=$((BUILD_END - BUILD_START))
-    zip_name="${maindir}/${KERNEL_NAME}${SUFFIX}-${TIME}-${commit}-${toolchain}-${ZIP_KERNEL_VERSION}.zip"
+    zip_name="${maindir}/${KERNEL_NAME}${SUFFIX}-${TIME}-${commit}-${ZIP_KERNEL_VERSION}.zip"
     pack ${zip_name}
     echo "build succeeded in $((DIFF / 60))m, $((DIFF % 60))s" >> "${zip_name}.info"
     echo "md5: $(md5sum "${zip_name}" | cut -d' ' -f1)" >> "${zip_name}.info"
+    echo "compiler: ${toolchain}" >> "${zip_name}.info"
   else
     BUILD_END=$(date +"%s")
     DIFF=$((BUILD_END - BUILD_START))
