cc898500160837acc3fcd8f8d2f8250936a371da
fukiame <fukiame@proton.me>
2023-09-04 09:22:08 +0700

    jfnenxjfbjs

--- ksu/patch.sh
+++ ksu/patch.sh
@@ -8,10 +8,7 @@ curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh
 KSU_GIT_VERSION=$(cd KernelSU && git rev-list --count HEAD)
 KERNELSU_VERSION=$(($KSU_GIT_VERSION + 10000 + 200))
 
-defconfig_file="${maindir}/arch/arm64/configs/${defconfig}"
-
-KERNEL_NAME=$(cat "${defconfig_file}" | grep "CONFIG_LOCALVERSION=" | sed 's/CONFIG_LOCALVERSION="-*//g' | sed 's/"*//g' )
-sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-$KERNEL_NAME-KSU-$KERNELSU_VERSION\"/" "${defconfig_file}"
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-KSU-$KERNELSU_VERSION\"/" "${defconfig_file}"
 
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
 
